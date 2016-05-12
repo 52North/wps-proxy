a6b13f4c1477bbfccddb387e5f6438c871881b5d
dewall <a.dewall@52north.org>
2016-05-12 08:30:17 +0200

    integrated POST-based soap wrapper for WPS Requests, setted json as the only return type

---
+++ RestfulWPSProxy-wps/src/main/java/org/n52/restfulwpsproxy/wps/SimplePostClient.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.n52.restfulwpsproxy.wps;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * TODO JavaDoc
+ *
+ * @author dewall
+ */
+public class SimplePostClient extends AbstractWPSClient {
+
+    /**
+     * Constructor.
+     *
+     * @param baseUrl
+     * @param restTemplate
+     */
+    public SimplePostClient(String baseUrl, RestTemplate restTemplate) {
+        super(baseUrl, restTemplate);
+    }
+
+    public String performPostRequest(String xmlRequest) {
+        HttpEntity requestEntity = new HttpEntity(xmlRequest, headers);
+        return restTemplate.exchange(baseUrl, HttpMethod.GET, requestEntity, String.class).getBody();
+    }
+}
